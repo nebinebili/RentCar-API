d661571a3a698e01947692eac4a60e4f849c61db
Nebi <nbilinbi7@gmail.com>
2022-11-09 02:10:47 +0400

    AOP ,ValidationAspect added

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constant;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,14 +25,14 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [ValidationAspect(typeof(CarValidator))]
+
         public IResult Add(Car car)
         {
-            if (car.DailyPrice > 0)
-            {
-                _carDal.Add(car);
-                return new SuccessResult(Messages.Added);
-            }
-            else return new ErrorResult(Messages.PriceInvalid);
+            ValidationTool.Validate(new CarValidator(), car);
+            _carDal.Add(car);
+            return new SuccessResult(Messages.Added);
+            
         }
 
         public IDataResult<List<Car>> GetAll()
